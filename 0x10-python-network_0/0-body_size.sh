8ecb0825070e9b36a77a26bb91d1163b33126bce
Haruna Bah <harunjibs@gmail.com>
2023-11-30 19:50:07 +0000

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# display size of the body of the res
-curl -s "$1" | wc -c
+# script that takes in a URL, sends a request to that URL, and displays size of the body of the response.
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
