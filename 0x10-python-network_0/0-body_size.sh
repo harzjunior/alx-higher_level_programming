1385dd9c2a2ce11e908b447bd6fb2da75485d491
Haruna Bah <harunjibs@gmail.com>
2023-11-30 19:39:10 +0000

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Displays the size of the body of the response in bytes
-size=$(curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
-echo "$size"
+# display size of the body of the res
+curl -s "$1" | wc -c
