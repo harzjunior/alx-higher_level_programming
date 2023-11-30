61dc69f51df7e13bd3d6c894c16193f0f2efbf89
Haruna Bah <harunjibs@gmail.com>
2023-11-30 19:47:51 +0000

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# take a URL & display all HTTP methods the server accepts
-curl -Is "$1" | grep "Allow: " | cut -d ' ' -f 2-
+# takes in a URL and displays all HTTP methods the server will accept
+curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
